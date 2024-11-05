5726222c057203369fa08713cad8a25c46d01384
ryanlll996 <ryan666leo@gmail.com>
2024-11-05 13:11:30 +0800

    update

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -95,7 +95,7 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        height: 2.75rem;
+        height: 3.25rem;
         padding: 0 0.75rem;
 
         .nav-bar__left {
@@ -150,7 +150,7 @@ export default {
             width: 30%;
             .logo {
                 margin-left: 0.3125rem;
-                height: 2.66rem;
+                height: 1.84rem;
                 display: flex;
                 align-items: center;
                 image {
